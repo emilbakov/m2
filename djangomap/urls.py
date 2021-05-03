b57559a1f3a32e9e83fd94dc59af219b39afce4d
unknown <eb.bakov@gmail.com>
2021-05-03 15:45:06 +0300

    Ui fix

--- djangomap/urls.py
+++ djangomap/urls.py
@@ -29,7 +29,10 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('listing/', include('listing.urls')),
-   
+    
+
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    
+    
     url(r'^data.geojson$', GeoJSONLayerView.as_view(model=HuntingSpot, properties=('title', 'description', 'picture_url','hardurl')), name='data')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- listing/urls.py
+++ listing/urls.py
@@ -1,11 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+from django.conf.urls import url
+
+
 
 from django.urls import path
 from . import views
 
 urlpatterns = [   
     path('', views.SpotListView.as_view(), name='spots'),
-    path('spot/<int:pk>/', views.SpotDetailView.as_view(), name='spot-detail'),     
+    path('spot/<int:pk>/', views.SpotDetailView.as_view(), name='spot-detail'),
+
+    path('map/', TemplateView.as_view(template_name='map.html'), name='map'),
+
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
