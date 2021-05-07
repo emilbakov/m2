9251f4f54691c9dc44c26048a155cd682d068c5d
unknown <eb.bakov@gmail.com>
2021-05-07 14:09:59 +0300

    email from the form

--- djangomap/view.py
+++ djangomap/view.py
@@ -1,7 +1,10 @@
 from djangomap.forms import ContactForm
 from django.views.generic.edit import FormView
+from django.core.mail import send_mail
 
 from django.urls import reverse, reverse_lazy
+
+
 
 
 class ContactFormView(FormView):
@@ -12,5 +15,23 @@ class ContactFormView(FormView):
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        form.send_email()
+
+        # subject = 'Website Inquiry' 
+		# body ={'name': form.cleaned_data['name'],'email': form.cleaned_data['email_address'],'message':form.cleaned_data['message']}
+        # message = "\n".join(body.values())
+
+		# try:
+		# 	send_mail(subject, message,'eb.bakov@gmail.com', ['eb.bakov@gmail.com']) 
+		# except BadHeaderError:
+		# 	return HttpResponse('Invalid header found.')
+		# return redirect ("main:homepage")      
+        print(form)
+        subject = 'Website Inquiry'
+        body ={'name': form.cleaned_data['name'],'email': form.cleaned_data['email'],'message':form.cleaned_data['message']}
+        message = "\n".join(body.values())
+        send_mail(subject,message,'eb.bakov@gmail.com', ['eb.bakov@gmail.com'])
+
+               
+                
         return super().form_valid(form)
+
