2622dae740ec3f6b715873e7802bfbacdaa02892
unknown <eb.bakov@gmail.com>
2021-05-08 12:20:30 +0300

    ImageAlbum

--- map/admin.py
+++ map/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 
 from . import models as hunting_models
 
+from .models import ImageAlbum ,Image
+
 
 
 
 admin.site.register(hunting_models.HuntingSpot, LeafletGeoAdmin)
+admin.site.register(ImageAlbum)
+admin.site.register(Image)
--- map/models.py
+++ map/models.py
@@ -4,6 +4,26 @@ from django.db import models
 # Used to generate URLs by reversing the URL patterns
 from django.urls import reverse
 
+def get_upload_path(instance, filename):
+    model = instance.album.model.__class__._meta
+    name = model.verbose_name_plural.replace(' ', '_')
+    return f'{name}/images/{filename}'
+
+class ImageAlbum(models.Model):
+    def default(self):
+        return self.images.filter(default=True).first()
+        
+    def thumbnails(self):
+        return self.images.filter(width__lt=100, length_lt=100)
+
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to=get_upload_path)
+    default = models.BooleanField(default=False)
+    width = models.FloatField(default=100)
+    length = models.FloatField(default=100)
+    album = models.ForeignKey(ImageAlbum, related_name='images', on_delete=models.CASCADE)
+
 
 class HuntingSpot(models.Model):
 
@@ -12,6 +32,7 @@ class HuntingSpot(models.Model):
     picture = models.ImageField()
     geom = GeometryCollectionField()
     hardurl = models.CharField(blank=True,null=True,max_length=256)
+    album = models.OneToOneField(ImageAlbum, related_name='model', on_delete=models.CASCADE, blank=True,null=True)
     
     def __str__(self):
         return self.title
